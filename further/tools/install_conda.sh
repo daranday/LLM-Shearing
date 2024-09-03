90495d8f7665664a7518ba466fcb8dafee960083
Daran He <daran.he@hpe.com>
2024-09-03 22:13:09 +0000

    Update code

--- further/tools/install_conda.sh
+++ further/tools/install_conda.sh
@@ -1,5 +1,9 @@
+# Check if 'conda' command is available
+if command -v conda &> /dev/null; then
+    echo "Conda is available. Exiting script."
+    exit 0
+fi
+
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -P /tmp
 bash /tmp/Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
-
 ~/miniconda3/bin/conda init
-conda create -n default python=3.10 -y
--- further/tools/provision.sh
+++ further/tools/provision.sh
@@ -2,6 +2,10 @@ cp -a assets/dotfiles/. ~/
 
 sh install_conda.sh
 
+conda init
+conda create -n default python=3.10 -y
+conda activate default
+
 # Source conda
 . ~/.bash_profile
 
@@ -15,3 +19,4 @@ conda install -c conda-forge \
 # Install pip packages
 pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
 pip install flash-attn==1.0.3.post
+pip install -e ../..
