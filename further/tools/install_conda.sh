7d7dae3bfdfdcfcd69164a963fcafecd6c15c4dd
Daran He <daran.he@hpe.com>
2024-09-03 20:27:46 +0000

    Update code

--- further/tools/install_conda.sh
+++ further/tools/install_conda.sh
@@ -2,4 +2,4 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
 
 ~/miniconda3/bin/conda init
-conda create -n default python=3.10
+conda create -n default --python=$(which python3)
