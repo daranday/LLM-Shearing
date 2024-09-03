1e3695c90ef6fdd7fded87f2a2bc187494ea8e2d
Daran He <daran.he@hpe.com>
2024-09-03 22:04:54 +0000

    update

--- further/tools/install_conda.sh
+++ further/tools/install_conda.sh
@@ -1,4 +1,4 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/Miniconda3-latest-Linux-x86_64.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -P /tmp
 bash /tmp/Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
 
 ~/miniconda3/bin/conda init
