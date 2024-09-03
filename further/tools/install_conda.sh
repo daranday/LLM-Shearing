817122d2deac563033edda7fc2fa9cdb70b5012a
Daran He <daran.he@hpe.com>
2024-09-03 20:16:49 +0000

    update provision.sh

--- further/tools/install_conda.sh
+++ further/tools/install_conda.sh
@@ -3,4 +3,3 @@ bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
 
 ~/miniconda3/bin/conda init
 conda create -n default python=3.10
-conda activate default
--- further/tools/provision.sh
+++ further/tools/provision.sh
@@ -1,3 +1,12 @@
 cp assets/.* ~/
 
 sh install_conda.sh
+
+# Source conda
+. ~/.profile
+
+# Install necessary packages
+conda install -c conda-forge \
+    htop \
+    tmux \
+    -y
