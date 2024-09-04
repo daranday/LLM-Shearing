784521b9a60e522c1aa8d1f89b3045a07c0551fb
Daran He <daran.he@hpe.com>
2024-09-04 20:42:05 +0000

    Update code

--- further/tools/provision.sh
+++ further/tools/provision.sh
@@ -19,5 +19,5 @@ conda install -c conda-forge \
 
 # Install pip packages
 pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
-pip install flash-attn==1.0.3.post
+MAKEFLAGS="-j$(nproc)" pip install flash-attn==1.0.3.post
 pip install -e ../..
