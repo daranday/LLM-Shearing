9ce6d0110327093a0d045061d7b7d539a0296bfb
Daran He <daran.he@hpe.com>
2024-09-16 18:36:15 -0500

    Update code

---
+++ further/experiments/ft_data_generation/config.py
@@ -0,0 +1,2 @@
+SLIMPAJAMA_ROOT = "/nvmefs1/mk/datasets/cerebras___slim_pajama-627_b"
+V2_REDPAJAMA_ROOT = "/nvmefs1/mk/datasets/redpajama-data-v2"
--- further/tools/install_pretraining_deps.sh
+++ further/tools/install_pretraining_deps.sh
@@ -3,3 +3,4 @@ pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2 --ind
 MAKEFLAGS="-j$(nproc)" pip install flash-attn==1.0.3.post
 pip install -e ../..
 pip install git+https://github.com/huggingface/transformers.git
+pip install datasets
